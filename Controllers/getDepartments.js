efb9827e4965ed3dfa674e7aef93f430bca64151
nae deshazer <naedeshazer@naes-MacBook-Pro.local>
2019-10-01 20:39:03 -0500

    settingItUp

---
+++ Controllers/getDepartments.js
@@ -0,0 +1,16 @@
+
+const mysql = require('mysql')
+const pool = require('../mysql/connections')
+const { handleSQLError } = require('../mysql/error')
+
+const getAllDepartments = (req, res) => {
+    let sql = "SELECT ?? FROM ??"
+    sql = mysql.format(sql, ["dept_name", "departments"])
+
+    pool.query(sql, (err, rows) => {
+        if (err) return handleSQLError(res, err)
+        return res.json(rows)
+    })
+}
+
+module.exports = { getAllDepartments }
--- mySQL/connection.js
+++ mySQL/connection.js
@@ -6,9 +6,9 @@ class Connection {
             console.log('creating connection...')
             this.pool = mysql.createPool({
                 connectionLimit: 100,
-                host: '35.232.187.33',
+                host: '35.239.116.71',
                 user: 'root',
-                password: 'admin3ACA',
+                password: 'iseanee1',
                 database: 'employees'
             })
             return this.pool
