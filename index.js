daea869fc575ffe8954732d7f1624e55421ba47a
nae deshazer <naedeshazer@naes-MBP.attlocal.net>
2019-10-02 02:57:08 -0500

    LastChangesLetMeKnowWhatYouThink

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const express = require('express')
-const app = express()
-const employeesRouter = require('./routes/employees');
-const port = process.env.PORT || 4001
+const express = require("express");
+const app = express();
+//const bodyParser = require('body-parser')
+const employeesRouter = require("./routes/employees");
+const salariesRouter = require("./routes/salaries");
+const departmentsRouter = require("./routes/departments")
+const titlesRouter = require('./routes/titles')
+const port = process.env.port || 4001;
 
 app.use(employeesRouter);
+app.use(salariesRouter);
+app.use(departmentsRouter);
+app.use(titlesRouter);
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the API');
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to our API");
+});
 
 app.listen(port, () => {
-    console.log('Welcome to our API:', port)
-  })
+  console.log(`Web server is listening on port ${port}!`);
+});
